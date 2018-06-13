d39b1a71493eb2738119d360ac4f1dd0fd946af1
Yuri K <39098541+yklav@users.noreply.github.com>
2018-06-13 11:40:43 -0300

    source

---
+++ .zsh/aliases.zsh
@@ -0,0 +1,14 @@
+# directory movement
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias 'bk=cd $OLDPWD'
+
+# Colorize output, add file type indicator, and put sizes in human readable format
+alias ls='ls -GFhl'
+
+# Same as above, but in long listing format
+alias ll='ls -GFhla'
+
+# Git
+alias gcl='git clone'
